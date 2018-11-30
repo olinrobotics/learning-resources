48c956d37b9b0dea6cd3a08db850240cf712d700
cweiss <charlievweiss@gmail.com>
2018-11-29 21:52:23 -0500

    unittests pass, need to decide input prior

--- kalman_filter/scripts/kalman_filter_1d.py
+++ kalman_filter/scripts/kalman_filter_1d.py
@@ -4,36 +4,36 @@ TODO:
 """
 from collections import namedtuple
 
+Gaussian = namedtuple('Gaussian', ['mean', 'variance'])
+Gaussian.__repr__ = lambda s: '𝒩(μ={:.3f}, 𝜎²={:.3f})'.format(s[0], s[1]) #Not necessary-- prints fancy symbols insteand of "mean" and "variance". Pretty cute though.
+
 # Pseudocode
 class KalmanFilter():
     @staticmethod
     def gaussian_multiply(g1, g2):
-        mean = (g1.var * g2.mean + g2.var * g1.mean) / (g1.var + g2.var)
-        variance = (g1.var * g2.var) / (g1.var + g2.var)
-        return gaussian(mean, variance)
+        mean = (g1.variance * g2.mean + g2.variance * g1.mean) / (g1.variance + g2.variance)
+        variance = (g1.variance * g2.variance) / (g1.variance + g2.variance)
+        return Gaussian(mean, variance)
 
-    def __init__(self, prior):
-        """All following measurements will be in relation to the starting point: The initial guess of where you are is extremely certain that it is where you started (a gaussian with a """
-        #helper code for gaussians:
-        gaussian = namedtuple('Gaussian', ['mean', 'variance'])
-        gaussian.__repr__ = lambda s: '𝒩(μ={:.3f}, 𝜎²={:.3f})'.format(s[0], s[1])
+    def __init__(self): #,prior)
+        """All following measurements will be in relation to the starting point: The initial guess of where you are is extremely certain that it is where you started (a Gaussian with a """
 
         #setup:
-        self.prior = gaussian(0,.001)
+        self.prior = Gaussian(0,.001)
 
     def predict(self,prior,movement):
         # uses old position (previous posterior) and adds expected movement, with uncertainty
-        x, P = prior.mean, prior.variance   # Unpack posterior gaussian
-        dx, Q = movement.mean, movement.variance    # Unpack movement gaussian
+        x, P = prior.mean, prior.variance   # Unpack posterior Gaussian
+        dx, Q = movement.mean, movement.variance    # Unpack movement Gaussian
         
         x = x + dx  # Update mean w/ predicted movement, no uncertainty
         P = P + Q   # Combine variances, decrease certainty
 
-        posterior = gaussian(x,P)
+        posterior = Gaussian(x,P)
         return posterior # new prior of expected position from movement
 
     def update(self,prior,measurement):
-        # gives final expected position based on gaussians of predicted place and new measurement
+        # gives final expected position based on Gaussians of predicted place and new measurement
         posterior = KalmanFilter.gaussian_multiply(measurement,prior)
         return posterior
 
@@ -48,13 +48,13 @@ class KalmanFilter():
         x = x + K*y      # posterior (this is where Kalman gain weights guess)
         P = (1 - K) * P  # posterior variance
 
-        posterior = gaussian(x,P)
+        posterior = Gaussian(x,P)
         return posterior
     """
 
     def step(self, z, dx, R=.001, Q=.001):
-        measurement = gaussian(z,R)
-        movement = gaussian(dx,Q)
+        measurement = Gaussian(z,R)
+        movement = Gaussian(dx,Q)
 
         new_prior = predict(self.prior, movement)
         posterior = update(new_prior,measurement)
---
+++ kalman_filter/scripts/kalman_filter_1d_test.py
@@ -0,0 +1,59 @@
+"""
+test whether predict and update work appropriately with random integers for mean and variance among initial prior, measurement, and movement
+"""
+
+import unittest
+from collections import namedtuple
+from neato_1d_kf import KalmanFilter
+import random
+
+
+Gaussian = namedtuple('Gaussian', ['mean', 'variance'])
+Gaussian.__repr__ = lambda s: '𝒩(μ={:.3f}, 𝜎²={:.3f})'.format(s[0], s[1]) #Not necessary-- prints fancy symbols insteand of "mean" and "variance". Pretty cute though.
+
+class KalmanFilter_Test(unittest.TestCase):
+    @staticmethod
+    def update(prior, measurement):
+        """Alternate method for gaussian multiplication to compare to one used in KalmanFilter"""
+        x, P = prior.mean, prior.variance        # mean and variance of prior
+        z, R = measurement.mean, measurement.variance  # mean and variance of measurement
+        
+        y = z - x        # residual (diff between prior and measurement)
+        K = P / (P + R)  # Kalman gain (calculated from Gaussian math -- weights new x based the difference in the trustworthiness of the prior and measurement)
+
+        x = x + K*y      # posterior (this is where Kalman gain weights guess)
+        P = (1 - K) * P  # posterior variance
+
+        posterior = Gaussian(x,P)
+        return posterior
+
+    def setUp(self):
+        self.KalmanFilter = KalmanFilter()
+
+    def test_predict(self):
+        dx = random.randint(0,1000) # movement step
+        Q = random.randint(0,1000) # movement variance
+
+        movement = Gaussian(dx,Q)
+
+        prior = Gaussian(random.randint(0,1000),random.randint(0,1000))
+        new_prior = self.KalmanFilter.predict(prior,movement)
+
+        self.assertEqual(prior.mean+movement.mean,new_prior.mean) # when means are equal, should be the same
+        self.assertEqual(prior.variance+movement.variance,new_prior.variance) # variances should add
+
+    def test_update(self):
+        z = random.randint(0,1000) #measurement
+        R = random.randint(0,1000) #faith in measurement
+
+        measurement = Gaussian(z,R)
+
+        prior = Gaussian(random.randint(0,1000),random.randint(0,1000))
+        new_prior = self.KalmanFilter.update(prior,measurement)
+
+        check_prior = KalmanFilter_Test.update(prior,measurement)
+
+        self.assertAlmostEqual(new_prior,check_prior)
+
+if __name__ == '__main__':
+    unittest.main()
